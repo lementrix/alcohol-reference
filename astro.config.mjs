6a87e602f0bb1c78d61653bc70085cb44defd7ee
Kostiantyn Yusupov <60828316+lementrix@users.noreply.github.com>
2026-01-26 10:55:04 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://ATG.github.io",
+  site: "https://lementrix.github.io",
   base: "/alcohol-reference/",
 });
